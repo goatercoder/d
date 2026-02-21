1e4422d4e28a06013c7964d2ce0c8fa3b6fa9641
goatercoder <camdenlchen@gmail.com>
2026-02-20 22:22:09 -0800

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,17 @@
 export async function handler() {
+  const response = await fetch(
+    "https://firestore.googleapis.com/v1/projects/wavelength-efd2d/databases/(default)/documents",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY}`
+      }
+    }
+  );
+
+  const data = await response.json();
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Backend is working ✅" })
+    body: JSON.stringify(data)
   };
 }
